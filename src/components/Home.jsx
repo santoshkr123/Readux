800a2e4a93e1ebbccf3e8b899134b34c1b5944d4
Santosh Kumar <santosh.rash44@gmail.com>
2024-09-29 12:48:15 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <div className="bg-gray-100 h-screen p-16">
-      <h1 className="text-4xl font-bold">CodingOtt</h1>
+      <h1 className="text-4xl font-bold">Coding</h1>
       <p className="text-xl font-semibold mt-2">Understanding Redux in React JS</p>
       <div className="space-x-8 mt-8">
         <Link to="/">Home</Link>
